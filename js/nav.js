e799fe231b3e55a27e6697169a02d678008537e9
Mike Kutilek <makutilek@gmail.com>
2018-06-20 20:06:15 -0400

    added forever, logging, and switching nav bar to angular

--- js/nav.js
+++ js/nav.js
@@ -1,17 +1,22 @@
 (function(){
 	var navApp = angular.module('nav-app', []);
+
+	navApp.controller('nav-ctrl', ['$scope', '$http', function($scope, $http){
+		$http.get('./config/nav.json').then(function(rawNavData){
+			/*
+			var navMap: object = {};
+
+			angular.forEach(rawNavData.data, function(navGroup, index){
+				navMap[navGroup.type] = navGroup;
+			});
+
+			$scope.navDataMap = navMap;
+			*/
+			$scope.navData = rawNavData.data;
+		})
+	}])
+
+	angular.bootstrap(document.getElementById("nav-app"), ['nav-app']);
 })();
-/*
-app.controller('nav-ctrl', ['$scope', '$http', '$window', function($scope, $http, $window){
-	$http.get('./config/nav.json').then(function(rawNavData){
-		var navMap: object = {};
 
-		angular.forEach(rawNavData.data, function(navGroup, index){
-			navMap[navGroup.type] = navGroup;
-		});
 
-		$scope.navDataMap = navMap;
-		$scope.navData = rawNavData.data;
-	})
-}])
-*/
