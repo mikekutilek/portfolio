82a9be99aaf320ecf8667889b6d7c92d22bccb79
mikekutilek <makutilek@gmail.com>
2018-10-28 20:31:33 -0400

    adding performance enhancements to opener

--- professional/SABR/opener.py
+++ professional/SABR/opener.py
@@ -66,37 +66,41 @@ def get_team_rotation_wOBA():
 	page = sa.get_page(position='SP', group_by='team', sort_col='woba')
 	data = sa.get_table(page)
 	data['wOBA'] = data['wOBA'].astype('float64')
-	return data.sort_values(by=['wOBA'], ascending=True)
+	return data
 
 def get_team_wOBA_chunk(team):
 	data = get_team_rotation_wOBA()
 	chunk_data = np.array_split(data, 5)
-	if (chunk_data[0]['Player'] == team).any():
+	if (chunk_data[4]['Player'] == team).any():
 		return "starting rotation is in the upper echelon of MLB in wOBA. They probably don't need an opener, but you can still take a look at potential options."
-	elif (chunk_data[1]['Player'] == team).any():
+	elif (chunk_data[3]['Player'] == team).any():
 		return "starting rotation is in one of the higher tiers of MLB in wOBA. They may or may not need an opener to fill a void, but you can still take a look at potential options."
 	elif (chunk_data[2]['Player'] == team).any():
 		return "starting rotation is in the middle of MLB in wOBA. They might need an opener to fill a void. Check out their options below!"
-	elif (chunk_data[3]['Player'] == team).any():
+	elif (chunk_data[1]['Player'] == team).any():
 		return "starting rotation is in one of the lower tiers of MLB in wOBA. They could probably use an opener to fill a void. See options below!"
-	elif (chunk_data[4]['Player'] == team).any():
+	elif (chunk_data[0]['Player'] == team).any():
 		return "starting rotation is at the very bottom of MLB in wOBA. They definitely have a rotation spot that could be better utilized. See options below!"
 	else:
 		return "Invalid team. Please try again."
 
 def get_rps_wOBA_vs(batter_stands):
-	pd.options.display.float_format = '${:,.3f}'.format
+	#pd.options.display.float_format = '${:,.3f}'.format
 	page = sa.get_page(batter_stands=batter_stands, position='RP', min_results='75', sort_col='woba')
 	data = sa.get_table(page)
-	data['wOBA'] = data['wOBA'].astype('float64').apply(lambda x: '{0:.3f}'.format(x))
+	new_data = pd.DataFrame()
+	new_data['Player'] = data['Player']
+	new_data['wOBA'] = data['wOBA'].astype('float64').apply(lambda x: '{0:.3f}'.format(x))
 	
-	return data.sort_values(by=['wOBA'], ascending=True).loc[data['wOBA'].astype('float64') < 0.250]
+	return new_data.loc[new_data['wOBA'].astype('float64') < 0.250]
 
 def get_sps_wOBA_vs(batter_stands):
 	page = sa.get_page(batter_stands=batter_stands, position='SP', hfInn='1%7C', min_results='30', sort_col='woba')
 	data = sa.get_table(page)
-	data['wOBA'] = data['wOBA'].astype('float64').apply(lambda x: '{0:.3f}'.format(x))
-	return data.sort_values(by=['wOBA'], ascending=False).loc[data['wOBA'].astype('float64') > 0.350]
+	new_data = pd.DataFrame()
+	new_data['Player'] = data['Player']
+	new_data['wOBA'] = data['wOBA'].astype('float64').apply(lambda x: '{0:.3f}'.format(x))
+	return new_data.loc[new_data['wOBA'].astype('float64') > 0.350]
 
 def get_all_candidates(batter_stands, position):
 	df = get_all_pitchers()
@@ -127,25 +131,25 @@ def main():
 	args = parser.parse_args()
 
 	rp_rdf = get_team_candidates(args.team, 'R', 'RP')
-	#rp_ldf = get_team_candidates(args.team, 'L', 'RP')
-	#sp_rdf = get_team_candidates(args.team, 'R', 'SP')
-	#sp_ldf = get_team_candidates(args.team, 'L', 'SP')
-	#chunk = get_team_wOBA_chunk(args.team)
+	rp_ldf = get_team_candidates(args.team, 'L', 'RP')
+	sp_rdf = get_team_candidates(args.team, 'R', 'SP')
+	sp_ldf = get_team_candidates(args.team, 'L', 'SP')
+	chunk = get_team_wOBA_chunk(args.team)
 	opener_data = {"chunk": "", "rp_righties": [], "rp_lefties": [], "sp_righties": [], "sp_lefties": []}
 
 	for index, r in rp_rdf.iterrows():
 		opener_data['rp_righties'].append(r.to_json())
 
-	#for index, l in rp_ldf.iterrows():
-	#	opener_data['rp_lefties'].append(l.to_json())
+	for index, l in rp_ldf.iterrows():
+		opener_data['rp_lefties'].append(l.to_json())
 
-	#for index, r in sp_rdf.iterrows():
-	#	opener_data['sp_righties'].append(r.to_json())
+	for index, r in sp_rdf.iterrows():
+		opener_data['sp_righties'].append(r.to_json())
 
-	#for index, l in sp_ldf.iterrows():
-	#	opener_data['sp_lefties'].append(l.to_json())
+	for index, l in sp_ldf.iterrows():
+		opener_data['sp_lefties'].append(l.to_json())
 
-	#opener_data['chunk'] = chunk
+	opener_data['chunk'] = chunk
 
 	print(json.dumps(opener_data))
 	#print(rdf)
